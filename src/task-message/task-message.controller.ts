0553cb46400c566cc22b2aff7bda12646213971b
Amirhosein-8993 <140063353+Amirhosein-8993@users.noreply.github.com>
2025-09-06 21:47:58 +0330

    feat: add create task-message

--- src/task-message/task-message.controller.ts
+++ src/task-message/task-message.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { TaskMessageService } from './task-message.service';
+import { CreateTaskMessageDto } from './task-message.dto';
 
-@Controller('task-message')
-export class TaskMessageController {}
+@UseGuards(AuthGuard)
+@Controller('task-messages')
+export class TaskMessageController {
+    constructor(private readonly taskMessageService: TaskMessageService) { }
+    @Post('')
+    create(@Request() req, @Body() data: CreateTaskMessageDto) {
+        return this.taskMessageService.create(req.user.sub, data);
+    }
+}
--- src/task-message/task-message.service.ts
+++ src/task-message/task-message.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { NameTask, PrismaClient } from 'generated/prisma';
+import { CreateTaskMessageDto } from './task-message.dto';
 
+const prisma = new PrismaClient()
 @Injectable()
-export class TaskMessageService {}
+export class TaskMessageService {
+    async create(user_id: number, { message }: CreateTaskMessageDto) {
+        await prisma.taskMessage.create({
+            data: {
+                message: message,
+                user_id: user_id,
+                task_id: 1
+            }
+        })
+        return { status: "ok" }
+    }
+}
