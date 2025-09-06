bbf309de3874d15fc31be1791d1cbc7d1c9d67ec
Amirhosein-8993 <140063353+Amirhosein-8993@users.noreply.github.com>
2025-09-06 21:46:36 +0330

    fix: update module task-message

--- src/task-message/task-message.module.ts
+++ src/task-message/task-message.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TaskMessageService } from './task-message.service';
+import { TaskMessageController } from './task-message.controller';
 
-@Module({})
+@Module({
+    controllers: [TaskMessageController],
+    providers: [TaskMessageService],
+})
 export class TaskMessageModule {}
